77e4c6b7675a1ea8cf30d7a3ff759f817d4edf79
qomaeng <qomaeng@gmail.com>
2025-02-14 23:22:34 +0900

    lazygit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -97,6 +97,25 @@ return {
       }
     end,
   },
+
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
 }
 
 -- {
